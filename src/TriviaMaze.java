aaf59918d9908140f27719f791915dc844d2e4db
Jinal Thummar <j63592@gmail.com>
2026-05-31 11:55:42 -0700

    Update TriviaMaze launcher

--- src/TriviaMaze.java
+++ src/TriviaMaze.java
@@ -1,32 +1,3 @@
-// import javax.swing.SwingUtilities;
-// import view.MazeGUI;
-
-// /**
-//  * Entry point for the Trivia Maze application.
-//  *
-//  * @author Suhayr Hassan
-//  * @author Jinal Thummar
-//  * @author Roman Pavlyshyn
-//  * @version 17 May 2026
-//  */
-// public final class TriviaMaze {
-
-//     /**
-//      * Private constructor to prevent instantiation.
-//      */
-//     private TriviaMaze() {
-//     }
-
-//     /**
-//      * Starts the program.
-//      *
-//      * @param theArgs command line arguments
-//      */
-//     public static void main(final String[] theArgs) {
-//         SwingUtilities.invokeLater(MazeGUI::createAndShowGUI);
-//     }
-// }
-
 /*
  * Trivia Maze - TCSS 360
  * Spring 2026
